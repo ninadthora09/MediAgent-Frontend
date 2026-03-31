f7af66104399af4b94d4babb7db86615dd3f486e
Ninad Thorat <programmingwithninadthorat4@gmail.com>
2026-03-31 14:24:15 +0530

    Backend

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("https://mediagent-backend.onrender.com", {
+      const response = await fetch(`${import.meta.env.BACKEND_API_URL}/chat`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
